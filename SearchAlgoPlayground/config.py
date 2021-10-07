0b12f796fda81635e0c3cc410c9ebfd1e3cadec3
Sritabh Priyadarshi <sobydanny@gmail.com>
2021-10-07 12:12:08 +0530

    config with dictionary

--- SearchAlgoPlayground/config.py
+++ SearchAlgoPlayground/config.py
@@ -1,12 +1,3 @@
-##Word Params###
-BLOCKS_DIMENSION = (21,23) #ROWS,COLUMNS
-
-GRID_WIDTH = 1
-TITLE = "Algo PlayGround"
-BOTTOM_PANEL_HEIGHT = 200 #Size of bottom panel for control
-MARGIN = 15 #Margin between sides and the grid
-
-
 ###Colors###
 DARK_PURPLE = (3, 3, 66)
 PURPLE = (136, 137, 219)
@@ -15,54 +6,32 @@ DARK_GRAY = (130, 130, 130)
 LIGHT_BLUE = (217, 247, 255)
 BROWN = (61, 51, 23)
 WHITE = (255,255,255)
-BLUE = (14, 113, 143)
+BLUE = (28,134,238)
 BLACK = (0,0,0)
 GREEN = (151, 240, 175)
 RED = (255, 120, 120)
 CYAN = (0, 255, 255)
 YELLOW = (217, 210, 74)
 
-##Some default colors
-BACKGROUND_COLOR = WHITE ##Color of the background of the world
-GRID_COLOR = GRAY #Block boundary color
-HIGHLIGHT_COLOR = DARK_GRAY #Highlighting color
-
-
-BUTTON_COLOR_PRIMARY = BROWN #Color for the button larger
-BUTTON_COLOR_SECONDARY = BLUE #color for the smaller button
-
-INFO_LABEL_COLOR = DARK_GRAY #Info label text color
-
-
-###Block Size###
-BLOCK_SIZE = 30 #Size of each block i.e. sides
-
-NODE_BORDER_COLOR = BLACK
-NODE_COLOR = GRAY
-SPECIAL_NODE_BORDER_COLOR = DARK_PURPLE
-SPECIAL_NODE_COLOR = GREEN
-SELECTED_NODE_COLOR = RED
-
-
-###FILE RESOURCES###
-MY_WORK_DIR = "MyGraph/" #Directory in which the work is saved
-
 
 config = {
     "TITLE":"Algo PlayGround",
-    "BLOCKS_DIMENSION":(21,23),
-    "BLOCK_SIZE":30,
-    "BOTTOM_PANEL_HEIGHT":200,
-    "MARGIN":15,
-    "GRID_WIDTH":1,
-    "BACKGROUND_COLOR":WHITE,
-    "GRID_COLOR":GRAY,
-    "HIGHLIGHT_COLOR":DARK_GRAY,
-    "BUTTON_COLOR_PRIMARY":BLUE,
-    "INFO_LABEL_COLOR":DARK_GRAY,
+    "BLOCKS_DIMENSION":(21,23),              #ROWS,COLUMNS
+    "BLOCK_SIZE":30,                         #Size of each block i.e. sides
+    "BOTTOM_PANEL_HEIGHT":200,               #Size of bottom panel for control
+    "MARGIN":15,                             #Margin between sides and the grid 
+    "GRID_WIDTH":1,                          #Width of the boundary of the block
+    "BACKGROUND_COLOR":WHITE,                #Color of the background of the world
+    "GRID_COLOR":GRAY,                       #Block boundary color
+    "HIGHLIGHT_COLOR":DARK_GRAY,             #Highlighting color
+    "BUTTON_COLOR_PRIMARY":BROWN,             #Color for the button larger
+    "BUTTON_COLOR_SECONDARY": BLUE,          #color for the smaller button
+    "INFO_LABEL_COLOR":DARK_GRAY,            #color for the info text
     "NODE_COLOR":GRAY,
-    "SPECIAL_NODE_BORDER_COLOR": DARK_PURPLE,
-    "SPECIAL_NODE_COLOR":GREEN,
+    "NODE_BORDER_COLOR":BLACK,
+    "SPECIAL_NODE_BORDER_COLOR": DARK_PURPLE,#Special node border color
+    "SPECIAL_NODE_COLOR":GREEN,              #special node color
     "SELECTED_NODE_COLOR" : RED,
-    "ELEMENT_ACTIVE_COLOR":BLUE
+    "ELEMENT_ACTIVE_COLOR":BLUE,             #Element selected by user is considered as active to the playground
+    "MY_WORK_DIR": "MyGraph/"                #Directory in which the work is saved
 }
