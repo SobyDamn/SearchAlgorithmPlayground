77825ec159c5098d8d3e449915af56329b6ce42e
Sritabh Priyadarshi <sobydanny@gmail.com>
2021-10-14 21:36:30 +0530

    removed

--- build/lib/SearchAlgoPlayground/Elements/__init__.py
+++
@@ -1,3 +0,0 @@
-from .blocks import Block
-from .nodes import Node
-from .edges import Edge
--- build/lib/SearchAlgoPlayground/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-from .UI import *
-from .Elements import Block,Node,Edge
-from .playground import PlayGround
-from .world import World
-from .config import config
